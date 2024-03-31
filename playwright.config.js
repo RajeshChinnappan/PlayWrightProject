c9ef5802953ef047b0ea3208c6f884e85388633f
Rajesh C <rajesh.c@reflectionsinfos.com>
2024-03-31 12:35:56 +0530

    code changes

--- playwright.config.js
+++ playwright.config.js
@@ -1,6 +1,24 @@
 // @ts-check
 const { defineConfig, devices } = require('@playwright/test');
 
+const capabilities = {
+  browserName: "Chrome",
+  browserVersion: "Latest",
+  "LT:Options": {
+      platform: "windows 10",
+      build: "Playwright Test Build",
+      name: "Playwright Testing",
+      user: "rajesh.creflectionsinfos",
+      accessKey: "oLVyCAWgWoTiJEfKb6X9i4IGIBSPBnxi4kKvNkz5VEaN85U5vL",
+      network: true,
+      video: true,
+      console: true,
+      tunnel: false,
+      tunnelName: "",
+      geoLocation: "",
+  },
+};
+
 /**
  * Read environment variables from file.
  * https://github.com/motdotla/dotenv
@@ -31,44 +49,46 @@ module.exports = defineConfig({
 
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
+    // connectOptions: {
+
+    //   wsEndpoint:  `wss://cdp.Lambdatest.com/playwright?capabilities=
+    //   ${encodeURIComponent(JSON.stringify(capabilities))}`
+    // },
     /* Base URL to use in actions like `await page.goto('/')`. */
     // baseURL: 'http://127.0.0.1:3000',
-   // baseURL: 'https://www.demoblaze.com/index.html',
+    baseURL: 'https://www.demoblaze.com/index.html',
+    screenshot: "only-on-failure",
+    video: "retain-on-failure",
+    trace: "on", headless: false
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
-    // screenshot: "only-on-failure",
-    // video: "retain-on-failure"
   },
+
 
   /* Configure projects for major browsers */
   projects: [
     {
       name: 'chromium',
-      use: { ...devices['Desktop Chrome'],screenshot: "on", video: "on", trace: "on", headless: false, baseURL: 'https://www.demoblaze.com/index.html',       },
+      use: { ...devices['Desktop Chrome']}
     },
 
     // {
     //   name: 'firefox',
-    //   use: { ...devices['Desktop Firefox'],screenshot: "on", video: "on", trace: "on", headless: false, baseURL: 'https://www.demoblaze.com/index.html',       },
+    //   use: { ...devices['Desktop Firefox']}
     // },
 
     // {
     //   name: 'webkit',
-    //   use: { ...devices['Desktop Safari'],screenshot: "on", video: "on", trace: "on", headless: false, baseURL: 'https://www.demoblaze.com/index.html',   },
+    //   use: { ...devices['Desktop Safari']}
     
     // },
 
     /* Test against mobile viewports. */
     // {
     //   name: 'Mobile Chrome',
-    //   use: { ...devices['Pixel 5'],screenshot: "on", video: "on", trace: "on", headless: false, baseURL: 'https://www.demoblaze.com/index.html',       },
+    //   use: { ...devices['Pixel 5']}
     // },
-    //  {
-    //   name: 'Mobile Safari',
-    //   use: { ...devices['iPhone 12'],screenshot: "on", video: "on", trace: "on", headless: false, baseURL: 'https://www.demoblaze.com/index.html',       },
-    // },
-
+    
   ],
 
   /* Run your local dev server before starting the tests */
